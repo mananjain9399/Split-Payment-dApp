e774e1580a93a11db02a21a05763e1d02a3730d0
mananjain9399 <jainmanash14@gmail.com>
2025-09-02 14:31:55 +0530

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,1 +1,17 @@
+// scripts/deploy.js
+
+const hre = require("hardhat");
+
+async function main() {
+  const Project = await hre.ethers.getContractFactory("Project");
+  const project = await Project.deploy();
+  await project.deployed();
+
+  console.log(`Contract deployed to: ${project.address}`);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
 
